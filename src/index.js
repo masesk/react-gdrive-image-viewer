1ef2f2980c3e4e5333481cd523ded75d91ccdac7
masesm5@yahoo.com <maseskrikorian@gmail.com>
2021-09-13 20:13:58 -0700

    test release

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ExampleGDrive from './example/ExampleGDrive'
-
-ReactDOM.render(<ExampleGDrive />, document.getElementById('root'));
